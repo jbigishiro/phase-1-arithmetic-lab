2a25a40be5cefc212f331a78a4a142564e4a7e4c
Justin Bigishiro <justin.rvd@gmail.com>
2023-03-17 01:14:40 -0500

    adds myfile

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-
-// Write your code here
+let num1=1;
+let num2=62;
+let multiply=num1*num2
+let random=Math.ceil(Math.random())
+let num3=9
+let num4=5
+let mod=num3%num4
+let max=Math.max(1,3,5,7,0,20)
